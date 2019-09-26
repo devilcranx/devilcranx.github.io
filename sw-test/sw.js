ff36a1f8c52c8834936a59285a59afab7fc6f68e
Viartsinski, Pavel <pviartsinski@scnsoft.com>
2019-09-26 13:53:09 +0300

    logs

--- sw-test/sw.js
+++ sw-test/sw.js
@@ -18,11 +18,12 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(caches.match(event.request).then(function(response) {
-    console.log('requset:', event.request);
-    console.log('response:', response);
+    //console.log('requset:', event.request);
+    //console.log('response:', response);
     // caches.match() always resolves
     // but in case of success response will have value
     if (response !== undefined) {
+      console.log('response:', response);
       return response;
     } else {
       return fetch(event.request).then(function (response) {
